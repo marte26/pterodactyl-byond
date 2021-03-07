6e9a8542d4818241772bdc72c14fb8b4c2f213e5
marte26 <martecet5067@gmail.com>
2021-03-07 19:10:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,5 @@ cd /home/container
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-if [ ${SERVER_HIDDEN} ]
-then
-  MODIFIED_STARTUP="${MODIFIED_STARTUP} -hidden"
-fi
-
 # Run the Server
 ${MODIFIED_STARTUP}
